419d3fab2161c09450f390d8f9a86e8ded64a591
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-11-02 18:01:49 -0300

    Project created

---
+++ backend/src/main/java/br/com/saltopetmanager/matheustirabassi/SaltoPetManager/SaltoPetManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.saltopetmanager.matheustirabassi.SaltoPetManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltoPetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltoPetManagerApplication.class, args);
+	}
+
+}
