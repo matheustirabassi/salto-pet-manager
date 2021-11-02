1f719b5c22ad4d3a29eab3e1f3019ffbefaf808c
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-11-02 20:39:43 -0300

    Testing Tutor class with REST

--- backend/src/main/java/br/com/saltopetmanager/matheustirabassi/SaltoPetManager/domain/Tutor.java
+++ backend/src/main/java/br/com/saltopetmanager/matheustirabassi/SaltoPetManager/domain/Tutor.java
@@ -1,5 +1,75 @@
 package br.com.saltopetmanager.matheustirabassi.SaltoPetManager.domain;
 
-public class Tutor {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
+public class Tutor implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long cpf;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private String gender;
+	
+	public Tutor() {
+	}
+	public Tutor(Long cpf, String name, String email, Date birthDate, String gender) {
+		this.cpf = cpf;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.gender = gender;
+	}
+	public Long getCpf() {
+		return cpf;
+	}
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tutor other = (Tutor) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+	
+	
+	
 }
--- backend/src/main/java/br/com/saltopetmanager/matheustirabassi/SaltoPetManager/resource/TutorResource.java
+++ backend/src/main/java/br/com/saltopetmanager/matheustirabassi/SaltoPetManager/resource/TutorResource.java
@@ -1,15 +1,27 @@
 package br.com.saltopetmanager.matheustirabassi.SaltoPetManager.resource;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.saltopetmanager.matheustirabassi.SaltoPetManager.domain.Tutor;
+
 @RestController
 @RequestMapping(value = "/tutors")
 public class TutorResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String  list() {
-		return "REST is working!";
+	public List<Tutor> list() throws NumberFormatException, ParseException {
+		List<Tutor> tutors = new ArrayList<>();
+		Tutor tutor1 = new Tutor(Long.parseLong("48386036818"), "Matheus", 
+				"tirabassi.matheus@aluno.ifsp.edu.br", new SimpleDateFormat("dd/MM/yyyy").parse("16/04/2000"), "M");
+		tutors.add(tutor1);
+		
+		return tutors;
 	}
 }
