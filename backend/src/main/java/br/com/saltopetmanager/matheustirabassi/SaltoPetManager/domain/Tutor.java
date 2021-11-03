65e33cfd385301a90806c6dabff151abf08c888d
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-11-02 21:22:43 -0300

    H2 Database added

--- backend/src/main/java/br/com/saltopetmanager/matheustirabassi/SaltoPetManager/domain/Tutor.java
+++ backend/src/main/java/br/com/saltopetmanager/matheustirabassi/SaltoPetManager/domain/Tutor.java
@@ -4,10 +4,17 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Tutor implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long cpf;
 	private String name;
 	private String email;
